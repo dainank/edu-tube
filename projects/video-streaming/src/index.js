173bc90c59c71e1da4e9a68327449e43af17257e
dainank <whelbe@outlook.com>
2023-04-17 18:53:47 +0200

    doc express

---
+++ projects/video-streaming/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express'); // load library
+
+const app = express();  // create instance
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
