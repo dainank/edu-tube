45e3759cf66634da2442baa25fb722c81723d169
dainank <whelbe@outlook.com>
2023-04-17 19:19:38 +0200

    cmdln args

--- projects/video-streaming/src/index.js
+++ projects/video-streaming/src/index.js
@@ -3,21 +3,25 @@ const fileSystem = require('fs');
 
 const app = express();  // create instance
 
-const port = 3000;  // production should be 80
- 
+if (!process.env.PORT) {
+    throw new Error("Missing PORT number! Use `export PORT=3000` or `set PORT=3000`")
+};
+
+const port = process.env.PORT;
+
 app.get("/video", async (req, res) => {
- 
+
     const path = "./assets/sample.mp4";
     const stats = await fileSystem.promises.stat(path);
- 
+
     res.writeHead(200, {
         "Content-Length": stats.size,
         "Content-Type": "video/mp4",
     });
-      
+
     fileSystem.createReadStream(path).pipe(res);
 });
- 
+
 app.listen(port, () => {
     console.log(`Microservice listening ...`);
 });
