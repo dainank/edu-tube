b5f53fb7bea1285db0af488c45ca5dac08e8589e
dainank <whelbe@outlook.com>
2023-04-24 13:45:14 +0200

    history microservice

---
+++ projects/video-history/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+const PORT = process.env.PORT;
+
+async function main() {
+
+    console.log("Hello world!");
+
+    const app = express();
+
+    app.listen(PORT, () => {
+        console.log("Microservice online.")
+    });
+}
+
+main()
+    .catch(err => {
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
