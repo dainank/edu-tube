3399e6cbc6800069812bd9f57241d611c0d3bbbe
dainank <whelbe@outlook.com>
2023-05-02 08:50:18 +0200

    resource grp instantiation

--- iterations/first/resource-group.tf
+++ iterations/first/resource-group.tf
@@ -1,7 +1,7 @@
 #
 # Creates a resource group automatically for edutube in my Azure account.
 #
-resource "azurerm_resource_group" "edutube" {
+resource "azurerm_resource_group" "edutube" { // resource grp
   name     = "edutube"
   location = "westeurope"
 }
