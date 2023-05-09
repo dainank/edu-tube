4973cbafdd63f80e9affc87f9e9eebd84dc004ce
dainank <whelbe@outlook.com>
2023-05-09 09:31:18 +0200

    minor adjustments

--- microservices/video-streaming/src/index.js
+++ microservices/video-streaming/src/index.js
@@ -1,71 +1,61 @@
-const express = require('express');
-const http = require('http');
-const mongodb = require("mongodb");
-
-const app = express();  // create instance
+const express = require("express");
+const fs = require("fs");
+const amqp = require('amqplib');
 
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
-if (!process.env.VIDEO_STORAGE_HOST) {
-    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
-}
-
-if (!process.env.VIDEO_STORAGE_PORT) {
-    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
-}
-
-if (!process.env.DBHOST) {
-    throw new Error("Please specify the databse host using environment variable DBHOST.");
-}
-
-if (!process.env.DBNAME) {
-    throw new Error("Please specify the name of the database using environment variable DBNAME");
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
 }
 
 const PORT = process.env.PORT;
-const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
-const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
-const DBHOST = process.env.DBHOST;
-const DBNAME = process.env.DBNAME;
+const RABBIT = process.env.RABBIT;
 
-console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
+//
+// Send the "viewed" to the history microservice.
+//
+function sendViewedMessage(messageChannel, videoPath) {
+    console.log(`Publishing message on "viewed" queue.`);
 
+    const msg = { videoPath: videoPath };
+    const jsonMsg = JSON.stringify(msg);
+    messageChannel.publish("", "viewed", Buffer.from(jsonMsg)); // Publish message to the "viewed" queue.
+}
+
+//
+// Application entry point.
+//
 async function main() {
-    const client = await mongodb.MongoClient.connect(DBHOST); // Connects to the database.
-    const db = client.db(DBNAME);   // retrieve DB
-    const videosCollection = db.collection("videos");   // retrieve collection
 
-    app.get("/video", async (req, res) => {
-        const videoId = new mongodb.ObjectId(req.query.id);
-        const videoRecord = await videosCollection.findOne({ _id: videoId });
-        if (!videoRecord) {
-            res.sendStatus(404);
-            return;
-        }
+    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
 
-        console.log(`Translated id ${videoId} to path ${videoRecord.videoPath}.`);
+    const messagingConnection = await amqp.connect(RABBIT) // Connect to the RabbitMQ server.
 
-        const forwardRequest = http.request( // Forward the request to the video storage microservice.
-            {
-                host: VIDEO_STORAGE_HOST,
-                port: VIDEO_STORAGE_PORT,
-                path: `/video?path=${videoRecord.videoPath}`, // Video path now retrieved from the database.
-                method: 'GET',
-                headers: req.headers
-            },
-            forwardResponse => {
-                res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-                forwardResponse.pipe(res);
-            }
-        );
+    console.log("Connected to RabbitMQ.");
 
-        req.pipe(forwardRequest);
+    const messageChannel = await messagingConnection.createChannel(); // Create a RabbitMQ messaging channel.
+
+    const app = express();
+
+    app.get("/video", async (req, res) => { // Route for streaming video.
+
+        const videoPath = "./assets/sample.mp4";
+        const stats = await fs.promises.stat(videoPath);
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+    
+        fs.createReadStream(videoPath).pipe(res);
+
+        sendViewedMessage(messageChannel, videoPath); // Send message to "history" microservice that this video has been "viewed".
     });
 
     app.listen(PORT, () => {
-        console.log(`Microservice listening, please load the data file db-fixture/videos.json into your database before testing this microservice.`);
+        console.log("Microservice online.")
     });
 }
 
